0b6bfe75aff4f2e5c22ba71e93afa6e62d20c793
KarlLodholz <KarlLodholz@gmail.com>
2020-08-12 20:42:38 -0500

    the algorithm can now handle any number of prime factors

--- pe_001.cpp
+++ pe_001.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-// #include <array>
-
 /* PROBLEM
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
+using namespace std;
+
+int gcd(int a, int b) {return (a ? gcd(b % a, a) : b);}  
 
 int main() {
     //variable init
     const int CAP = 1000;
-    const int ROOTS[2] = {3,5};
+    const int ROOTS[] = {3,5};
     int num =0, sum = 0;
 
     //attempt 1
@@ -27,13 +28,32 @@ int main() {
     //attempt 2
     sum = 0;
     int n;
-    const int LEN = sizeof(ROOTS)/sizeof(ROOTS[0]);
-    for(int i=0; i<LEN; i++) {
-        int n = (CAP-1)/ROOTS[i];
+    for(int i=0; i<2; i++) {
+        n = (CAP-1)/ROOTS[i];
         sum += (int)(ROOTS[i] * (n/2.0) * (n+1));
     }
     n = (CAP-1) / (ROOTS[0]*ROOTS[1]);
     sum -= (int)((ROOTS[0]*ROOTS[1]) * (n/2.0) * (n+1));
+    std::cout<< sum <<std::endl;
+
+    //attempt 3
+    //with this attempt the size of the array isn't limited to two integers
+    sum = 0;
+    const int LEN = sizeof(ROOTS)/sizeof(ROOTS[0]);
+    int lcm;
+    for(int i=0; i<LEN; i++) {
+        n = (CAP-1)/ROOTS[i];
+        sum += (int)(ROOTS[i] * (n/2.0) * (n+1));
+    }
+
+    for(int i=0; i<LEN-1; i++) {
+        for(int j=i+1; j<LEN;j++) {
+            lcm = (ROOTS[i] * ROOTS[j]) / gcd(ROOTS[i],ROOTS[j]);
+            n = (CAP-1)/(lcm);
+            sum -= (int)(lcm * (n/2.0) * (n+1));
+        }
+    }
+
     std::cout<< sum <<std::endl;
 
 }
