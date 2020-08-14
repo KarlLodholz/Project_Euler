324dc8ce1fc3fd190d5af3864e353a1fd453aaca
KarlLodholz <karllodholz@gmail.com>
2020-08-14 12:32:55 -0500

    increased precision

--- timer.h
+++ timer.h
@@ -19,11 +19,11 @@ public:
 
     void stop() {
         auto timeEnd = std::chrono::high_resolution_clock::now();
-        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_timeStart).time_since_epoch().count();
-        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(timeEnd).time_since_epoch().count();
+        auto start = std::chrono::time_point_cast<std::chrono::nanoseconds>(m_timeStart).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::nanoseconds>(timeEnd).time_since_epoch().count();
         auto time = end - start;
-        double ms = time * 0.001;
-        std::cout<<time<<"us ("<<ms<<"ms)"<<std::endl;
+        double ms = time * 0.000001;
+        std::cout<<time<<"ns ("<<ms<<"ms)"<<std::endl;
     };
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> m_timeStart;
